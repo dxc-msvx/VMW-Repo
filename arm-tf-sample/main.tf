e57a76746610fd3cb0810405a8debc0f59c40175
dxc-msvx <95429320+dxc-msvx@users.noreply.github.com>
2021-12-03 14:01:37 +0100

    Create main.tf

---
+++ arm-tf-sample/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_template_deployment" "storage_account_arm" {
+  name                = var.storage_account_deployment_name
+  resource_group_name = var.storage_account_resource_group_name
+  deployment_mode     = "Incremental"
+  template_body       = file("Modules/tf-storageaccount-arm/templates/storage-account.json",)
+  parameters = {
+    StorageAccountName      = var.storage_account_StorageAccountName
+    storageAccountType      = var.storage_account_storageAccountType
+    location                = var.storage_account_location
+    StorageAccountKind      = var.storage_account_StorageAccountKind
+  }
+}
