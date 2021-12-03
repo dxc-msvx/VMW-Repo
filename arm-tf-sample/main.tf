c62803e2babf3bd8d64cd18d3508430afe51501b
dxc-msvx <95429320+dxc-msvx@users.noreply.github.com>
2021-12-03 19:46:28 +0100

    Update main.tf

--- arm-tf-sample/main.tf
+++ arm-tf-sample/main.tf
@@ -1,7 +1,4 @@
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you're using version 1.x, the "features" block is not allowed.
+rovider "azurerm" {
     version = "~>2.0"
     features {}
 }
