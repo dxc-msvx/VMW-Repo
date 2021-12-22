b7eda655c8e74770096f149df4a7f2caaf03eb7b
dxc-msvx <95429320+dxc-msvx@users.noreply.github.com>
2021-12-22 18:30:32 +0100

    Create main.tf

---
+++ TF-ARM - Deploy a Storage Account/main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_template_deployment" "terraform-arm" {
+  name                = "terraform-arm-01"
+  resource_group_name = azurerm_resource_group.terraform-arm.name
+
+  template_body = file("template.json")
+
+  parameters = {
+    "storageAccountName" = "terraformarm"
+    "storageAccountType" = "Standard_LRS"
+
+  }
+
+  deployment_mode = "Incremental"
+}
